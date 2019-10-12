e05b9cbab185e148d815254f5c81609a171ed66f
paulrajib <rajib.ece09@gmail.com>
2019-10-12 22:00:22 +0600

    SearchBar UI just before state

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import SearchBar from './SearchBar';
 
 class App extends React.Component{
 	render(){
-		return <div><SearchBar /></div>
+		return (
+			<div className="ui container">
+				<SearchBar />
+			</div>
+		);
 	}
 }
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,7 +4,16 @@ import React from 'react';
 class SearchBar extends React.Component{
 
 	render(){
-		return <div>Search Bar</div>;
+		return (
+			<div className="searc-bar ui segment">
+				<form className="ui form">
+					<div className="field">
+						<label>Video Search</label>
+						<input type="text" />
+					</div>
+				</form>
+			</div>
+		);
 	}
 }
 
